8731d0fb6f6f3eab6d8339a86d7fb5d892687387
tigge <758184041@qq.com>
2020-07-07 22:01:05 +0800

    Initial commit

---
+++ src/main/java/hello.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: w
+ * Date: 2020/7/7
+ * Time: 22:00
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("gf");
+    }
+}
