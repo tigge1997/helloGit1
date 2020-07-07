a8d5d9acb144185217fa2e595e0018757882090e
tigge <758184041@qq.com>
2020-07-07 22:48:37 +0800

    分支bug1提交

---
+++ src/main/java/bug1.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: w
+ * Date: 2020/7/7
+ * Time: 22:48
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class bug1 {
+}
