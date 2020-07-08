2c60b8eb9ee663c89e0b989ec1efeb946e54f0e2
tigge <758184041@qq.com>
2020-07-08 10:29:56 +0800

    es第一次

--- src/main/java/EclipseHello.java
+++ src/main/java/EclipseHello.java
@@ -4,7 +4,7 @@ public class EclipseHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("eclipse");
+		System.out.println("eclipse1");
 	}
 
 }
